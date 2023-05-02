1d4717e9cb5a287b08c1e0a617aca2419ead40d6
John Doe <pavelaleksin90@gmail.com>
2023-05-02 11:34:27 +0200

    Move

---
+++ Codewars python.py
@@ -0,0 +1,9 @@
+# def square_digits(num):
+    # return (''.join(str(int(i)**2) for i in str(num)))
+
+# def find_it(seq):
+    # s = [int(seq.count(i)) for i in seq]
+    # for n in s:
+        # if n % 2 != 0:
+            # return seq[s.index(n)]
+
